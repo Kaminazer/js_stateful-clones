2063ed7dfffd56c7cd22e0271a55f329a53a1b94
Nazarii Korol <nazarkorol3684@gmail.com>
2025-05-13 13:26:05 +0300

    Add solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,67 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const transformHistory = [];
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      let transformResult;
+
+      if (transformHistory.length > 0) {
+        transformResult = cloneLastTransform(transformHistory.at(-1));
+      } else {
+        transformResult = cloneLastTransform(state);
+      }
+
+      for (const field in action.extraData) {
+        transformResult[field] = action.extraData[field];
+      }
+
+      transformHistory.push(transformResult);
+    }
+
+    if (action.type === 'removeProperties') {
+      let transformResult;
+
+      if (transformHistory.length > 0) {
+        transformResult = cloneLastTransform(transformHistory.at(-1));
+      } else {
+        transformResult = cloneLastTransform(state);
+      }
+
+      for (const key of action.keysToRemove) {
+        delete transformResult[key];
+      }
+
+      transformHistory.push(transformResult);
+    }
+
+    if (action.type === 'clear') {
+      let transformResult;
+
+      if (transformHistory.length > 0) {
+        transformResult = cloneLastTransform(transformHistory.at(-1));
+      } else {
+        transformResult = cloneLastTransform(state);
+      }
+
+      for (const field in transformResult) {
+        delete transformResult[field];
+      }
+
+      transformHistory.push(transformResult);
+    }
+  }
+
+  return transformHistory;
+}
+
+function cloneLastTransform(source) {
+  const clone = {};
+
+  Object.assign(clone, source);
+
+  return clone;
 }
 
 module.exports = transformStateWithClones;
